dc07c75b0220bd7f58dadb408b5d2662f496d5b1
supercoderhawk <supercoderhawk@gmail.com>
2019-06-03 10:24:19 +0800

    add unittest and data

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+import os
+
+__base_dir = os.path.dirname(__file__)
+TEST_DATA_DIR = os.path.realpath(os.path.join(__base_dir, 'test_data')) + '/'
---
+++ tests/io/test_file.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+from python_arsenal.io.file import *
+from tests import TEST_DATA_DIR
+
+
+def test_read_lines():
+    filename = TEST_DATA_DIR + 'a.txt'
+    lines = read_lines(filename)
+    true_lines = ['This is an example.   ',
+                  'This is an different example.',
+                  '',
+                  '    Hahaha.']
+    assert lines == true_lines
+    skip_lines = read_lines(filename, skip_empty=True)
+    assert skip_lines == [l for l in true_lines if l]
+    assert read_lines(TEST_DATA_DIR + 'a.txt.gbk', 'gbk') == ['你好', '这是一个例子。']
+
+
+def test_read():
+    filename = TEST_DATA_DIR + 'a.txt'
+    text = read_file(filename)
+    true_text = "This is an example.   \nThis is an different example.\n\n    Hahaha."
+    assert text == true_text
+
+
+def test_read_json():
+    read_json(TEST_DATA_DIR + 'a.json')
+
+
+def test_text_file():
+    true_text = "This is an example.   \nThis is a different example.\n\n    Hahaha."
+    text_file = TextFile(TEST_DATA_DIR + 'a.txt')
+    text = text_file.read()
+    lines = text_file.read_lines()
+    assert text == true_text
+    assert lines == []
+    expected_write_text = 'New Example\n\n'
+    text_file.write(expected_write_text)
+    print(text_file.read())
+    assert text_file.read() == expected_write_text
+    expected_write_lines = ['A', '', 'BBB\n', 'C']
+    text_file.write_lines(expected_write_text)
+    assert text_file.read_lines(False, False) == expected_write_lines
+
+    text_file.write(true_text)
