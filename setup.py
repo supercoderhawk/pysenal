79bd040a1e314e4c038b6c303b501025d99a5da1
supercoderhawk <supercoderhawk@gmail.com>
2021-03-10 14:50:47 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-    Setup file for pysenal.
+from setuptools import setup, find_packages
 
-    This file was generated with PyScaffold 2.5.11, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
-"""
+VERSION = "0.1.0"
 
-import sys
-from setuptools import setup
+requirments = []
 
-
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
-
-
-if __name__ == "__main__":
-    setup_package()
+setup(
+    install_requires=requirments,
+    version=VERSION
+)
